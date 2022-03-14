213b590b1ce50b1fff27b67d83c0a42150707dac
shray0027 <59202921+shray0027@users.noreply.github.com>
2022-03-14 21:20:09 +0530

    initial commit

---
+++ IAmRich/IAmRich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmRich
+//
+//  Created by Shray Anand on 13/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
